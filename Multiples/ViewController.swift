6192a2a4692ad66514ff49c3266fcb07747bbba5
BenRaskin <b-jammin@b-jammins-mbp.home>
2015-12-02 00:37:57 -0800

    initial upload

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var counterLabel: UILabel!
+  
+    
+   
+        
+        
+        
+  
+
+    @IBAction func addButtonClicked(sender: AnyObject) {
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playButtonClicked(sender: AnyObject) {
+        
+        
     }
-
-
 }
 
